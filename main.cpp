79991b3f8f5f81ca5c4466d64c061e75b8d6aef9
Brighten <bwan006@ucr.edu>
2020-05-18 04:57:51 -0700

    sudoku board initializer (set as function?)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+using namespace std;
 int main(){
-  cout << "test" << endl;
+  //goal is to make global sudoku board and have it displayed, then we'll deal with everything else.
+  string board [9][9];
+  for(int i = 0; i < 9; i++){
+      for(int j = 0; j < 9; j++){
+        board[i][j] = "-";
+      }
+  }
+  for(int i = 0; i < 9; i ++){
+    for (int j = 0; j < 9; j++){
+      cout << board[i][j] << " ";
+    }
+    cout << endl;
+  }
+
   return 0;
-  aasodfji
-  
+
 }
